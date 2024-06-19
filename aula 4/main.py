3053e1b946b75dea8698cd677b3ee585f55e55fe
Alejandro Alberoni <atralberoni@inf.ufpel.edu.br>
2024-06-19 16:40:54 -0300

    main loop

--- aula 4/main.py
+++ aula 4/main.py
@@ -4,27 +4,30 @@ def main():
     calc = Calculadora()
 
     # Inserir laço para ele repetir até o usuário decidir sair
-    print("Calculadora Simples")
-    print("===================")
-    
-    a = float(input("Digite o primeiro número: "))
-    b = float(input("Digite o segundo número: "))
-    
-    operacao = input("Escolha a operação (+, -, *, /): ")
-    
-    if operacao == '+':
-        resultado = calc.adicionar(a, b)
-    elif operacao == '-':
-        resultado = calc.subtrair(a, b)
-    elif operacao == '*':
-        resultado = calc.multiplicar(a, b)
-    elif operacao == '/':
-        resultado = calc.dividir(a, b)
-    else:
-        print("Operação inválida.")
-        return
+    while True:
+        print("Calculadora Simples")
+        print("===================")
 
-    print(f"O resultado de {a} {operacao} {b} é: {resultado}")
+        a = float(input("Digite o primeiro número: "))
+        b = float(input("Digite o segundo número: "))
+
+        operacao = input("Escolha a operação (+, -, *, /): ")
+
+        if operacao == '+':
+            resultado = calc.adicionar(a, b)
+        elif operacao == '-':
+            resultado = calc.subtrair(a, b)
+        elif operacao == '*':
+            resultado = calc.multiplicar(a, b)
+        elif operacao == '/':
+            resultado = calc.dividir(a, b)
+        else:
+            print("Operação inválida.")
+            return
+
+        print(f"O resultado de {a} {operacao} {b} é: {resultado}")
+        if(input("Deseja continuar? (s/n): ") == 'n'):
+            break
 
 if __name__ == "__main__":
     main()
